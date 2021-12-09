bc64ff77f76911ecfe7c039ae9ce3a7aeaef0c6f
Umed <example@mail.ru>
2021-12-09 03:01:02 +0300

    Lesson 2

--- js/main.js
+++ js/main.js
@@ -10,7 +10,23 @@ const personalMovieDB = {
   privat: false,
 };
 
-let questionFirst = prompt("один из последних просмотренных фильмов?", "");
-let questionSecond = prompt("На сколько оцените его?", "");
+for (let i = 0; i < 2; i++) {
+  const a = prompt("Один из последних просмотренных фильмов?", ""),
+    b = prompt("На сколько оцените его?", "");
 
-personalMovieDB.movies[questionFirst] = questionSecond;
+  if (a !== null && b !== null && a.length <= 50 && a !== "" && b !== "") {
+    personalMovieDB.movies[a] = b;
+  } else {
+    i--;
+  }
+}
+
+let count = personalMovieDB.count;
+
+if (count < 10) {
+  console.log("Просмотрено довольно мало фильмов");
+} else if (count > 10 && count < 30) {
+  console.log("Вы классический зритель");
+} else {
+  console.log("Вы киноман");
+}
